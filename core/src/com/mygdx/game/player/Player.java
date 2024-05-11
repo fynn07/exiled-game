f2258b9dbea1ab67c690f0ae8b16bff239f17054
Fynn Borja <pewdiestuff@gmail.com>
2024-05-11 17:49:50 +0800

    Modularized Handling Player Movement

--- core/src/com/mygdx/game/player/Player.java
+++ core/src/com/mygdx/game/player/Player.java
@@ -1,9 +1,23 @@
 package com.mygdx.game.player;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mygdx.game.animation.Animator;
+import com.mygdx.game.screens.Play;
 
-public class Player {
+public class Player{
+    private Animator animator;
+    private SpriteBatch spriteBatch;
+    private float stateTime;
+
+    public Player(){
+        animator = new Animator();
+        spriteBatch = new SpriteBatch();
+        stateTime = 0f;
+    }
 
     public Texture createPlayer(){
         return new Texture(Gdx.files.internal("assets/Characters/Male/Male_0_Idle0.png"));
@@ -32,4 +46,46 @@ public class Player {
     public Texture runEast(){
         return new Texture(Gdx.files.internal("assets/Characters/Animations/run_E.png"));
     }
+
+    //TODO: Change to handle players
+    public void handleMovement() {
+        stateTime += Gdx.graphics.getDeltaTime() * 0.5f;
+        TextureRegion currentFrame = null;
+
+        boolean isMovingDiagonal = false;
+
+        // Check for diagonal movement
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+                currentFrame = animator.animate(runNorthWest()).getKeyFrame(stateTime, true);
+                isMovingDiagonal = true;
+            } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+                currentFrame = animator.animate(runNorthEast()).getKeyFrame(stateTime, true);
+                isMovingDiagonal = true;
+            } else {
+                currentFrame = animator.animate(runNorth()).getKeyFrame(stateTime, true);
+            }
+        } else if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+            if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+                currentFrame = animator.animate(runSouthWest()).getKeyFrame(stateTime, true);
+                isMovingDiagonal = true;
+            } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+                currentFrame = animator.animate(runSouthEast()).getKeyFrame(stateTime, true);
+                isMovingDiagonal = true;
+            } else {
+                currentFrame = animator.animate(runSouth()).getKeyFrame(stateTime, true);
+            }
+        } else if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            currentFrame = animator.animate(runWest()).getKeyFrame(stateTime, true);
+        } else if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            currentFrame = animator.animate(runEast()).getKeyFrame(stateTime, true);
+        }
+
+        spriteBatch.begin();
+        if (currentFrame != null) {
+            spriteBatch.draw(currentFrame, 50, 50);
+        }
+        spriteBatch.end();
+    }
+
 }
